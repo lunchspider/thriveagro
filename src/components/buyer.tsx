95f0ee9683ab4939a1a60593439100258efa6b64
ayuugoyal <ayushgoyal8178@gmail.com>
2024-03-31 23:38:30 +0530

    Ad

--- src/components/buyer.tsx
+++ src/components/buyer.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import styles from './buyer.module.css'; // Import CSS module
+import React from "react";
+import styles from "./buyer.module.css"; // Import CSS module
+import { Home, Tractor } from "lucide-react";
 // import Link from 'next/link';
 const Buyer: React.FC = () => {
   return (
@@ -7,19 +8,19 @@ const Buyer: React.FC = () => {
       <h1>Start to contribute with reliable farmers</h1>
       <h2>We help you grow healthier, higher-quality crops.</h2>
       <div className={styles.farmcard}>
-      <div className={styles.card}>
-        <img src="/images/buyer.jpg" alt="House Icon" />
-        <h3>Join as a Buyer</h3>
-        <p>
-          Join our supportive community and learn from experienced farmers.
-        </p>
-        <button>Join as a Buyer</button>
-      </div>
-      <div className={styles.card}>
-        <img src="/images/farmer.jpg" alt="Person Icon" />
-        <h3>Join as a Farmer</h3>
-        <p>Share your knowledge and expertise to help others succeed.</p>
-        <button>Join as a Farmer</button> 
+        <div className={styles.card}>
+          <Home />
+          <h3>Join as a Buyer</h3>
+          <p>
+            Join our supportive community and learn from experienced farmers.
+          </p>
+          <button>Join as a Buyer</button>
+        </div>
+        <div className={styles.card}>
+          <Tractor />
+          <h3>Join as a Farmer</h3>
+          <p>Share your knowledge and expertise to help others succeed.</p>
+          <button>Join as a Farmer</button>
         </div>
       </div>
     </div>
